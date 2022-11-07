40a97be8301950e780cfb0cb12713459d9bdcda5
deval281shah <deval281shah@gmail.com>
2022-11-07 09:24:23 -0800

    added dataset related files

---
+++ hpe_resnet50/datasets/300W_LP/run_crop_300lp.sh
@@ -0,0 +1,8 @@
+python crop_300lp.py --db './AFW' --img_size 224 --ad 0.6
+python crop_300lp.py --db './AFW_Flip' --img_size 224 --ad 0.6
+python crop_300lp.py --db './HELEN'  --img_size 224 --ad 0.6
+python crop_300lp.py --db './HELEN_Flip'  --img_size 224 --ad 0.6
+python crop_300lp.py --db './IBUG'  --img_size 224 --ad 0.6
+python crop_300lp.py --db './IBUG_Flip'  --img_size 224 --ad 0.6
+python crop_300lp.py --db './LFPW'  --img_size 224 --ad 0.6
+python crop_300lp.py --db './LFPW_Flip'  --img_size 224 --ad 0.6
---
+++ hpe_resnet50/datasets/AFLW2000/run_crop_AFLW2000.sh
@@ -0,0 +1,1 @@
+python crop_AFLW2000.py --db './' --img_size 224 --ad 0.6
